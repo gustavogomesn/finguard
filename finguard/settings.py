91118f55c920f38e61eafdd14c54782595a1a464
Gustavo Gomes <gustavo.gonosi@gmail.com>
2024-10-18 18:06:11 -0300

    Disabled debug mode

--- finguard/settings.py
+++ finguard/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -121,5 +121,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login'
 
 CSRF_TRUSTED_ORIGINS = [
-    
+    "https://finguard-2c9984f2afaf.herokuapp.com/"
 ]
